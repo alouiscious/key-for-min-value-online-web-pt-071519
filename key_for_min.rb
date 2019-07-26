e3be302371b8d9fab37923f43f040cbb773490a6
Renee Sims <info@reneesoul.com>
2019-07-26 16:27:33 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -14,7 +14,7 @@ def key_for_min_value(hash)
   hash.each do |key, value|
     # if hash.empty?
     #   return nil
-    if min_value == nil || value < min_value 
+    if min_value == 0 || value < min_value 
       min_value = value
       min_key = key
     end
